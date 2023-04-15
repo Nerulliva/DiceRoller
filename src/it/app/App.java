41a7ad3ad05b12a8b00ef707cebb8785d68607d4
Ivano Cappelli <Ivano@192.168.1.60>
2023-04-15 20:17:05 +0200

    primo commit

---
+++ src/it/app/App.java
@@ -0,0 +1,46 @@
+package it.app;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class App {
+
+    private JFrame frame;
+
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    App window = new App();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public App() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 350, 450);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+
+
+    }
+}
